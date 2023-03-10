ea46545d3782184b9b3fa2a53c0458d05ef698dd
pmoncaiozb <126477143+pmoncaiozb@users.noreply.github.com>
2023-03-10 11:41:54 +0000

    Pending changes exported from your codespace

--- find_day.py
+++ find_day.py
@@ -1,1 +1,8 @@
+import datetime
+import calendar
 
+def find_day(date):
+    kon = datetime.datetime.strptime(date, '%d %m %Y').weekday()
+    return calendar.day_name[kon]
+
+print("The day of the week is " + find_day("11 10 2023"))
